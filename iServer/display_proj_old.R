407b86d11ac8f586e5990ff504a26699a22b4821
x2007tro <kemin.business@gmail.com>
2020-04-06 11:27:52 -0300

    Add instruction back to task list

--- iServer/display_proj_old.R
+++ iServer/display_proj_old.R
@@ -72,21 +72,8 @@ output$task_output <- renderUI({
                                   tags$div(class = "task_div", textInput(paste0("stp",k), label = "Step", value = step_k, width = entry_wid_s)),
                                   tags$div(class = "task_div", textInput(paste0("tsk",k), label = "Task", value = task_k, width = entry_wid_xl)),
                                   tags$div(class = "task_div", textInput(paste0("act",k), label = "Action", value = action_k, width = entry_wid_m)),
-                                  tags$div(
-                                    class = "inst_div",
-                                    id = paste0("comt", k),
-                                    lapply(1:length(comt_k), function(j){
-                                      if(comt_k[j] == "") tags$p("None") else tags$p(comt_k[j])
-                                    })
-                                  ),
-                                  tags$div(
-                                    class = "inst_div",
-                                    id = paste0("fcomt", k),
-                                    lapply(1:length(fcomt_k), function(j){
-                                      if(comt_k[j] == "") tags$p("None") else tags$p(fcomt_k[j])
-                                    })
-                                  )
-                                  # add more tag here
+                                  tags$div(class = "task_div", id = "instr_div", tags$a(id = "instr_a", href=paste0("#comt",k), "Instructions"))
+                                  
                                 )
                               }
                             )
@@ -102,7 +89,80 @@ output$task_output <- renderUI({
               )
             )
           )
+        ),
+        
+        ##
+        # Instruction section
+        fluidRow(
+          column(
+            width = 12,
+            
+            tags$div(
+              class = "block_outter_frame",
+              tags$h3(
+                class = "block_title",
+                "Instructions"
+              ),
+              tags$div(
+                class = "block_inner_frame",
+                
+                ##
+                # task output
+                lapply(1:length(tasks), function(k){
+                  
+                  ##
+                  # Process data, create year and month data
+                  step_k <- tasks[[k]]$step
+                  comt_k <- tasks[[k]]$comt
+                  fcomt_k <- tasks[[k]]$fcomt
+                  
+                  ##
+                  # Main task section
+                  fluidRow(
+                    column(
+                      width = 12,
+                      
+                      tags$div(
+                        style = "border-bottom:1px solid gray;",
+                        
+                        ##
+                        # meat here
+                        if(tasks[[k]]$step == "#") {
+                          
+                          tags$h4(class = "task_cat_div", tasks[[k]]$task)
+                          
+                        } else {
+                          
+                          tagList(
+                            tags$div(class = "task_div", textInput(paste0("inst_stp",k), label = "Step", value = step_k, width = entry_wid_s)),
+                            tags$div(
+                              class = "inst_div",
+                              id = paste0("comt", k),
+                              lapply(1:length(comt_k), function(j){
+                                if(comt_k[j] == "") tags$p("None") else tags$p(comt_k[j])
+                              })
+                            ),
+                            tags$div(
+                              class = "inst_div",
+                              id = paste0("fcomt", k),
+                              lapply(1:length(fcomt_k), function(j){
+                                if(comt_k[j] == "") tags$p("None") else tags$p(fcomt_k[j])
+                              })
+                            ),
+                            tags$div(class = "task_div", id = "instr_div", tags$a(id = "instr_a", href=paste0("#stp",k), "Go back"))
+                          )
+                          
+                        }
+                      )
+                    )
+                  )
+                  
+                })
+              )
+            )
+          )
         )
+        
     ),
     
     tabPanel(
